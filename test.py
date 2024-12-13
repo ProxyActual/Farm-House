bb47b5cd50af86496f8eecc574dd262def501313
ProxyActual <curtis@loginet.com>
2024-12-13 14:41:05 -0800

    Got a new element

--- test.py
+++ test.py
@@ -8,32 +8,29 @@ def random_walk(start, length):
 start_number = 100.0
 wallet_baseCoin = 100.0
 wallet_tradeCoin = 0
-array_length = 30000
+array_length = 30000 * 2
 random_array = random_walk(start_number, array_length)
 random_array[random_array < 1] = 1
 
 import matplotlib.pyplot as plt
 
-window_size = 1000
+window_size = 500
 averaged_array = np.convolve(random_array, np.ones(window_size)/window_size, mode='valid')
 plt.plot(averaged_array, label='Averaged Array')
 peaks, _ = find_peaks(averaged_array)
 
 valleys, _ = find_peaks(-averaged_array)
-plt.plot(valleys, averaged_array[valleys], "o", label='Valleys')
-
-plt.plot(peaks, averaged_array[peaks], "x")
 
 blank_array = np.zeros_like(random_array)
 
 filtered_peaks = []
 filtered_valleys = []
 
-for peak in peaks:
-    for valley in valleys:
-        if abs(averaged_array[peak] - averaged_array[valley]) >= 4:
-            filtered_peaks.append(peak)
-            filtered_valleys.append(valley)
+for i in range(len(peaks)):
+    if((averaged_array[peaks[i]] - averaged_array[valleys[i]]) > 10):
+        filtered_peaks.append(peaks[i])
+        filtered_valleys.append(valleys[i])
+        print("Peak: ", peaks[i], "Valley: ", valleys[i])
 
 peaks = np.array(filtered_peaks)
 valleys = np.array(filtered_valleys)
@@ -48,12 +45,19 @@ for i in range(len(random_array)):
         wallet_baseCoin = 0
     
     blank_array[i] = wallet_baseCoin + (wallet_tradeCoin * random_array[i]/100.0) * .95
-plt.plot(random_array, label='Random Array')
 
+plt.plot(random_array, label='Random Array')
 plt.plot(blank_array, label='Wallet Value')
-time = array_length * 15 / 60 / 24
+
+plt.plot(valleys, averaged_array[valleys], "o", label='Valleys')
+
+plt.plot(peaks, averaged_array[peaks], "x")
+
+time = array_length * 15 / 60 / 24 / 365
 print("Time: ", time)
+print("Wallet Value: ", wallet_baseCoin + wallet_tradeCoin * random_array[-1]/100.0, " Increase of ", (wallet_baseCoin + wallet_tradeCoin * random_array[-1]/100.0) - start_number)
 plt.legend()
 plt.show()
+
 
 
